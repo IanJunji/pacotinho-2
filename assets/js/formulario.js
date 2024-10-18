094a0602f19dac01f3306a95119cee468b5611f1
Ian Junji <ianmatsushita@gmail.com>
2024-10-17 23:10:12 -0300

    feat

---
+++ assets/js/formulario.js
@@ -0,0 +1,28 @@
+
+
+function assuntos(){
+    
+    
+
+
+
+    var assuntos = document.querySelector("Assuntos").ariaValueMax
+
+
+    switch (assuntos) {
+    case "Adoção":
+    break;
+    case "Como ajudar?":
+    break;
+    case "Próximos eventos":
+    break;
+    case "Sugestões":
+    break;
+    case "Outros":
+    break;
+    }
+}
+
+function enviar(){
+
+}
