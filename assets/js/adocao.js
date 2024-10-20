aa0f2a7cfda4bef0cebf391322a9f048d9e4dc10
DevCilas <160146962+DevCilas@users.noreply.github.com>
2024-10-20 16:07:18 -0300

    feat - adocao_js created

---
+++ assets/js/adocao.js
@@ -0,0 +1,10 @@
+function descricaoLeo(){
+    
+    alert("Este é o Leo! Um macho muito dócil de aproxiamdamente 2 anos!")
+
+}
+function descricaoMia(){
+    
+    alert("Esta é a Mia, uma gata!")
+
+}
